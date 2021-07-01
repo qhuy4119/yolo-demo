2414c315f375af52bce5f576b2bad1d7a85105a0
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-07-01 15:33:26 +0700

    handle empty upload

--- web-demo/app.py
+++ web-demo/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for, redirect
 from flask import render_template, request
 from .detector import predict
 import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
@@ -15,7 +14,14 @@ def hello_world():
 def detect():
     uploaded_img = request.files["uploaded-img"]
     imgSavePath = "static/user-uploaded-img.jpg"
-    uploaded_img.save(imgSavePath)
+    isNewUpload = True
+    if uploaded_img.filename == '':
+        if not os.path.isfile(imgSavePath):
+            return redirect(url_for("error"))
+        else:
+            isNewUpload = False
+    else:
+        uploaded_img.save(imgSavePath)
     if request.form["choose-model"] == "original-model":
         print("Using original yolo")
         objects = predict(imgSavePath, "original")
@@ -24,9 +30,13 @@ def detect():
         objects = predict(imgSavePath, "custom")
     numObjects = len(objects)
     listObjects = [(obj["name"], round(obj["confidence"]*100, 2)) for obj in objects]
-    return render_template("detect.html", numObjects=numObjects, listObjects=listObjects)
+    return render_template("detect.html", isNewUpload=isNewUpload, numObjects=numObjects, listObjects=listObjects)
 
 @app.route("/models_info")
 def models_info():
     trainImages = os.listdir("static/train")
     return render_template("models_info.html", trainImages=trainImages)
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
