fd1bdcc44666397511bd8e35beb4f3d3d28c4eee
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-07-02 10:44:04 +0700

    user can specify batch, subdivisions, width, height in cfg file

--- utils/yolov4-tinyCustomConfig.py
+++ utils/yolov4-tinyCustomConfig.py
@@ -5,6 +5,7 @@
 import subprocess
 import os, shutil
 
+
 def get_obj_data(classes):
     cwd = os.getcwd()
     backupPath = input("Enter path of backup dir: ")
@@ -18,11 +19,15 @@ def get_obj_data(classes):
     return objectData
 
 
-def write_cfg_file(classes):
+def write_cfg_file(classes, batch, subdivisions, width, height):
     commandAndArgs = ["sed", "-i"]
     filename = ["yolov4-tiny-obj.cfg"]
     expressions = [
-        [r"/^max_batches/c\max_batches=%s" % (classes * 2000)],
+        [r"/^batch/c\batch=%s" % batch],
+        [r"/^subdivisions/c\subdivisions=%s" % subdivisions],
+        [r"/^width/c\width=%s" % width],
+        [r"/^height/c\height=%s" % height],
+        [r"/^max_batches/c\max_batches=%s" % max(6000, (classes * 2000))],
         [
             r"/^steps/c\steps=%s,%s"
             % (int(classes * 2000 * 80 / 100), int(classes * 2000 * 90 / 100))
@@ -36,16 +41,20 @@ def write_cfg_file(classes):
         subprocess.run(commandAndArgs + e + filename)
 
 
-def write_files(objectData, classes):
+def write_files(objectData, classes, batch, subdivisions, width, height):
     with open("obj.data", "w") as f:
         for k, v in objectData.items():
             f.write("%s = %s\n" % (k, v))
-    write_cfg_file(classes)
+    write_cfg_file(classes, batch, subdivisions, width, height)
 
 
 if __name__ == "__main__":
     classes = int(input("Enter number of classes: "))
+    batch = int(input("Enter batch number (default is 64) : "))
+    subdivisions = int(input("Enter subdivisions (default is 1): "))
+    width = int(input("Enter network image width (default is 416): "))
+    height = int(input("Enter network image height (default is 416): "))
     print("IN THE NEXT SECTION: ALL PATHS ARE RELATIVE TO THE DARKNET EXECUTABLE\n")
     objectData = get_obj_data(classes)
-    write_files(objectData, classes)
+    write_files(objectData, classes, batch, subdivisions, width, height)
     shutil.copyfile("classes.txt", "obj.names")
